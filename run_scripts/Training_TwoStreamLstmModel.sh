bc3426ca7315a1e9b06b4d9bffa816e6ebb7308d
rextang-TencentPC1 <yongyi.tang92@gmail.com>
2018-07-07 17:44:01 +0800

    add fire config

--- run_scripts/Training_TwoStreamLstmModel.sh
+++ run_scripts/Training_TwoStreamLstmModel.sh
@@ -1,6 +1,22 @@
-export CUDA_VISIBLE_DEVICES='3'
+export CUDA_VISIBLE_DEVICES='0'
+export AUTH_URL="http://10.242.152.100:8008/checkAuthorityForSZHadoop.php"
+export AUTH_HDFS_URL="http://10.242.152.100:8008/checkHdfsPathAuthority.php"
+export RTX_NAME=matthzhuang
+export BUS_NAME=VideoAI
+export TOKEN=e7412d14-f0e8-43a2-a84c-23fa418d5399
+export HADOOP_HDFS_HOME='./hadoop-2.5.1'
+export HADOOP_HOME=$HADOOP_HDFS_HOME
+export HADOOP_CONF_DIR="$HADOOP_HDFS_HOME/etc/hadoop"
+export HADOOP_CLASSPATH="`$HADOOP_HOME/bin/hadoop classpath`"
+export CLASSPATH="$HADOOP_CLASSPATH:$CLASSPATH"
+for i in `find ${HADOOP_HOME} -name "*.jar"`
+do
+       export CLASSPATH="$i:$CLASSPATH"
+done
+export http_proxy=http://10.223.133.20:52107
+export https_proxy=http://10.223.133.20:52107
 python ../train.py \
---train_data_pattern='/data1/rextang/yt8m/frame/*.tfrecord' \
+--train_data_pattern='hdfs://100.110.18.133:9000/user/VideoAI/rextang/yt8m_2018/frame/*.tfrecord' \
 --model=twoStreamLstmModel \
 --train_dir=twoStreamLstmModel-0002-val-150-random \
 --frame_features=True \
